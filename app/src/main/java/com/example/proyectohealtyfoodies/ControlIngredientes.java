27bff077e810e02e22444f0df1cb955b97710b39
diego ortega <diego-chido2011@hotmail.com>
2022-10-19 10:32:21 -0600

    Modifico la lista de los ingredientes

--- app/src/main/java/com/example/proyectohealtyfoodies/ControlIngredientes.java
+++ app/src/main/java/com/example/proyectohealtyfoodies/ControlIngredientes.java
@@ -12,8 +12,8 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ControlIngredientes extends AppCompatActivity {
-    private ListView languageLV;
-    private Button addBtn;
+    private ListView listaIngredientes;
+    private Button btnAgregar;
     private EditText itemEdt;
     private ArrayList<String> lngList;
 
@@ -21,41 +21,32 @@ public class ControlIngredientes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_ingredientes);
-        // on below line we are initializing our variables.
-        languageLV = findViewById(R.id.idLVLanguages);
-        addBtn = findViewById(R.id.idBtnAdd);
+        listaIngredientes = findViewById(R.id.idLVLanguages);
+        btnAgregar = findViewById(R.id.idBtnAdd);
         itemEdt = findViewById(R.id.idEdtItemName);
-        lngList = new ArrayList<>();
+        lngList = new ArrayList<>(); // Cada Ingrediente se almacena en un ArrayList
 
-        // on below line we are adding items to our list
+        /* items de Ejemplo
         lngList.add("C++");
-        lngList.add("Python");
+        lngList.add("Python");*/
 
-        // on the below line we are initializing the adapter for our list view.
+        // Estamos inicializando el adaptador para nuestra vista de lista.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lngList);
 
-        // on below line we are setting adapter for our list view.
-        languageLV.setAdapter(adapter);
+        // Establecemos el adaptador en la lista
+        listaIngredientes.setAdapter(adapter);
 
-        // on below line we are adding click listener for our button.
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // on below line we are getting text from edit text
-                String item = itemEdt.getText().toString();
-
-                // on below line we are checking if item is not empty
-                if (!item.isEmpty()) {
-
-                    // on below line we are adding item to our list.
-                    lngList.add(item);
-
-                    // on below line we are notifying adapter
-                    // that data in list is updated to
-                    // update our list view.
+                // Obtenemos el texto del edittext
+                String ingrediente = itemEdt.getText().toString();
+                if (!ingrediente.isEmpty()) {
+                    lngList.add(ingrediente);
+                    // notificamos al adaptador que si se actualizan datos en la lista
+                    // se debe actualizar la vista de la lista de los ingredientes
                     adapter.notifyDataSetChanged();
                 }
-
             }
         });
     }
