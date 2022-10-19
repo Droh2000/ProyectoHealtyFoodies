8e07dc01989e31932be243ba59c9cb18927f10f8
diego ortega <diego-chido2011@hotmail.com>
2022-10-18 20:26:53 -0600

    Cambios Despues de agregar la parte de los ingredientes

---
+++ app/src/main/java/com/example/proyectohealtyfoodies/ControlIngredientes.java
@@ -0,0 +1,62 @@
+package com.example.proyectohealtyfoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ControlIngredientes extends AppCompatActivity {
+    private ListView languageLV;
+    private Button addBtn;
+    private EditText itemEdt;
+    private ArrayList<String> lngList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_ingredientes);
+        // on below line we are initializing our variables.
+        languageLV = findViewById(R.id.idLVLanguages);
+        addBtn = findViewById(R.id.idBtnAdd);
+        itemEdt = findViewById(R.id.idEdtItemName);
+        lngList = new ArrayList<>();
+
+        // on below line we are adding items to our list
+        lngList.add("C++");
+        lngList.add("Python");
+
+        // on the below line we are initializing the adapter for our list view.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lngList);
+
+        // on below line we are setting adapter for our list view.
+        languageLV.setAdapter(adapter);
+
+        // on below line we are adding click listener for our button.
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting text from edit text
+                String item = itemEdt.getText().toString();
+
+                // on below line we are checking if item is not empty
+                if (!item.isEmpty()) {
+
+                    // on below line we are adding item to our list.
+                    lngList.add(item);
+
+                    // on below line we are notifying adapter
+                    // that data in list is updated to
+                    // update our list view.
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+        });
+    }
+}
